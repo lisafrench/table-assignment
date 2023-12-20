b3ff25531a0cb9a664ee51dc93153b7753eb98e6
Lisa French <frenchlaf@gmail.com>
2023-12-20 08:23:43 -0600

    Update styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Table Assignment</h1>
+      <h1 className="text-center">Table Assignment</h1>
       <Table></Table>
     </>
   );
